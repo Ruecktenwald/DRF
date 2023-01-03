6be52aa400f2ddd024c4274b60048af862594372
Peter Ruecktenwald <ruecktenwald@gmail.com>
2023-01-03 10:51:19 -0500

    add permissions - allowany

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+RestFramework = {
+    'DEFAULT_PERMISSION_CLASSES': 
+    [ 'rest_framework.permissions.AllowAny', ]
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
